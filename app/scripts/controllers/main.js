bb9a9e784464bf2a4277d8dcc635949ac7383a30
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-22 16:14:46 +0000

    can now remove a todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,8 @@ angular.module('todoChallengeApp')
       $scope.todos.push($scope.todo);
       $scope.todo = '';
   };
+
+   $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
 });
