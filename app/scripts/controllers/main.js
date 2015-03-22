96bc8a3cd3ced41533cf97731396198a65ea417d
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-22 16:07:58 +0000

    can now add a todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,10 @@
 'use strict';
+
 angular.module('todoChallengeApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [];
-  });
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+  };
+});
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -17,4 +17,10 @@ describe('Controller: MainCtrl', function () {
   it('should have no items to start', function () {
     expect(scope.todos.length).toBe(0);
   });
+
+  it('should add items to the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
 });
