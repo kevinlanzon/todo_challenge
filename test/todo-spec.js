453817d33521cb6e09e21158efe21be639e086eb
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-23 17:57:13 +0000

    tidied syntax

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -8,11 +8,9 @@ describe('angularjs homepage todo list', function() {
     expect(browser.getTitle()).toEqual('My Todo List');
   });
 
-
   it('should add a todo', function() {
     element(by.model('todo')).sendKeys('write a protractor test');
     element(by.css('[value="Add"]')).click();
-
     var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(1);
     expect(todoList.get(0).$('input[type="text"]').getAttribute('value')).toEqual('write a protractor test');
@@ -21,7 +19,6 @@ describe('angularjs homepage todo list', function() {
   it('should remove a todo', function() {
     element(by.model('todo')).sendKeys('delete me!');
     element(by.css('[value="Add"]')).click();
-
     var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(2);
     element(by.css('[aria-label="Remove"]')).click();
