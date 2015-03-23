f08ca78801bfd47f7e102ac77d44fe42bd2abcc1
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-23 18:18:14 +0000

    all protractor tests written and passing

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -1,7 +1,8 @@
 describe('angularjs homepage todo list', function() {
+    var todoList = element.all(by.repeater('todo in todos'));
 
   beforeEach(function() {
-     browser.get('http://localhost:9000/#/');
+    browser.get('http://localhost:9000/#/');
   });
 
   it('should have a title', function() {
@@ -11,7 +12,6 @@ describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
     element(by.model('todo')).sendKeys('write a protractor test');
     element(by.css('[value="Add"]')).click();
-    var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(1);
     expect(todoList.get(0).$('input[type="text"]').getAttribute('value')).toEqual('write a protractor test');
   });
@@ -19,10 +19,21 @@ describe('angularjs homepage todo list', function() {
   it('should remove a todo', function() {
     element(by.model('todo')).sendKeys('delete me!');
     element(by.css('[value="Add"]')).click();
-    var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(2);
     element(by.css('[aria-label="Remove"]')).click();
     expect(todoList.count()).toEqual(1);
+  });
+
+  it('should navigate to the about page from the homepage', function() {
+    element(by.partialLinkText("About")).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:9000/#/about');
+  });
+
+  it('should navigate to the homepage from the about page', function() {
+    element(by.partialLinkText("About")).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:9000/#/about');
+    element(by.partialLinkText("Home")).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:9000/#/');
   });
 
 });
