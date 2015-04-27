c236d24f7b41e278b323b3e079734d6d64922d04
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-27 10:11:21 +0100

    modified gruntfile to pass grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,16 +28,16 @@ module.exports = function (grunt) {
 
     protractor: {
     options: {
-      configFile: "test/conf.js", // Default config file
+      configFile: 'test/conf.js', // Default config file
       keepAlive: true, // If false, the grunt process stops when the test fails.
       noColor: false, // If true, protractor will not use colors in its output.
       args: {
         // Arguments passed to the command
       }
     },
-    your_target: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
+    yourTarget: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
       options: {
-        configFile: "test/conf.js", // Target-specific config file
+        configFile: 'test/conf.js', // Target-specific config file
         args: {} // Target-specific arguments
       }
     },
